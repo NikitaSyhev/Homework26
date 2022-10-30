7408a101d42b6cd602e012f517fed9d42580a1aa
Nikita Sychev <nikitasychev@inbox.ru>
2022-10-30 10:01:50 +0500

    Done Task2

--- Main.cpp
+++ Main.cpp
@@ -8,7 +8,10 @@ struct movies {
 	std::string genre;
 	unsigned int duration;
 	unsigned int price;
-} m1{ "About hockey", 1980, "Biography", 120, 20 };
+} m1{ "About hockey", 1980, "Biography", 120, 20 },
+m2{"Travel around the world", 2000, "Traveling", 100, 25},
+m3{"Predator", 1990, "Action", 100, 15}
+;
 
 
 void show_movie(movies& P) {
@@ -19,8 +22,18 @@ void show_movie(movies& P) {
 	std::cout << "Price: " << P.price<< '\n';
 }
 
+movies expensive(movies &P1, movies& P2, movies& P3) {
+	
+	if (P1.price > P2.price && P1.price > P3.price)
+		return m1;
+	else
+		if (P2.price > P1.price && P2.price > P3.price)
+			return m2;
+		else
+			return m3;
+}
 
-
+update_info(m1);
 
 int main() {
 	setlocale(LC_ALL, "Russian");
@@ -31,6 +44,13 @@ int main() {
 
 	std::cout << "Task 1: Функция  show_movie.\n";
 	show_movie(m1);
+
+	std::cout << "Task 2: Функция  expensive.\n";
+	expensive(m1, m2, m3);
+
+	std::cout << "Task 3. Функция update_info.";
+	
+
 
 
 
