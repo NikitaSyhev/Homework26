ebddf8a99920cf4efadae495ff115fe9b3fa96f6
Nikita Sychev <nikitasychev@inbox.ru>
2022-10-30 09:03:12 +0500

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,34 @@
+//Этот проект создан с помощью классического шаблона
+#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int m, n;
+
+
+	struct movies {
+		std::string title = "name";
+		int year;
+		std::string genre;
+		unsigned int duration;
+		unsigned int price;
+	};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
